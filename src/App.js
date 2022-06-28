cb745f73ece08be653794366768fdf1725732448
Raymond Poon <raymondpoon@Raymonds-MacBook-Pro.local>
2022-06-29 00:00:11 +0100

    added route for checkout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./routes/home/Home";
 import Navigation from "./routes/Navigation/Navigation";
 import Authentication from "./routes/Authentication/Authentication";
 import Shop from "./routes/shop/Shop";
+import Checkout from "./routes/Checkout/Checkout";
 
 const App = () => {
   return (
@@ -17,8 +18,9 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
+                <Route path="shop" element={<Shop />} />
                 <Route path="auth" element={<Authentication />} />
+                <Route path="checkout" element={<Checkout />} />
               </Route>
             </Routes>
           </Router>
